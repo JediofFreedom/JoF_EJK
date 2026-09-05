43f68acbeab8298616aa96d2e81ad256f7049a12
lumaya <89487897+lumayaa@users.noreply.github.com>
2026-09-05 10:20:56 +0200

    Restore selected skin after team modes

--- codemp/cgame/cg_players.c
+++ codemp/cgame/cg_players.c
@@ -2474,16 +2474,8 @@ void CG_NewClientInfo( int clientNum, qboolean entitiesInitialized ) {
 
 	if (cgs.gametype >= GT_TEAM	&& !cgs.jediVmerc && cgs.gametype != GT_SIEGE )
 	{ //We won't force colors for siege.
+		//Team skins are transient; keep the archived model cvar unchanged so non-team games restore the player's chosen skin.
 		BG_ValidateSkinForTeam( newInfo.modelName, newInfo.skinName, newInfo.team, newInfo.colorOverride );
-
-		if (clientNum == cg.clientNum)
-		{
-			//Fix required to make the profile menu show the right team colors if someone has randomly written /model <skinname>/<wrongTeam>
-			char modelAndSkin[MAX_QPATH];
-			snprintf(modelAndSkin, sizeof(modelAndSkin), "%s/%s", newInfo.modelName, newInfo.skinName);
-			trap->Cvar_Set("model", modelAndSkin);
-		}
-
 	}
 	else
 	{
